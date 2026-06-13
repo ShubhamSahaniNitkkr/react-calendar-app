f7885189443fefbd37010db1a4b7ffeeb40a4c7b
Shubham Sunny <shubhamsahaninitkkr@gmail.com>
2026-06-13 13:02:12 +0530

    Fix CI build: set CI=false in GitHub Pages workflow

--- src/App.js
+++ src/App.js
@@ -8,21 +8,27 @@ import {
   WorkWeek,
   Month,
   Agenda,
-  EventSettingsModel,
 } from '@syncfusion/ej2-react-schedule';
 
 class App extends React.Component {
   state = {
     dataSource: [
       {
+        Id: 1,
+        Subject: 'Sample Event',
         EndTime: new Date(2020, 7, 11, 6, 30),
         StartTime: new Date(2020, 7, 11, 4, 0),
       },
     ],
   };
+
   render() {
     return (
-      <ScheduleComponent eventSettings={this.state.dataSource}>
+      <ScheduleComponent
+        height="100%"
+        selectedDate={new Date(2020, 7, 11)}
+        eventSettings={{ dataSource: this.state.dataSource }}
+      >
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
       </ScheduleComponent>
     );
